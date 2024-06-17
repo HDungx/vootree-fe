9c1fd4b6cc3461dd11daa7ad26f37c782d8b25f3
Nguyen Thuy Ngoc Giau <Giauntn0205@gmail.com>
2024-06-17 15:18:18 +0700

    CodeCuaDao

---
+++ app/partner/room-management/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useState } from "react";
+import Forms_Room from "./components/form";
+import Tables_Room from "./components/table";
+
+export default function Page() {
+  const [selectedRow, setSelectedRow] = useState<any | null>(null);
+
+  const handleRowSelect = (row: any) => {
+    setSelectedRow(row);
+  };
+  return (
+    <div>
+      <div className="mt-10">
+        <Tables_Room onRowSelect={handleRowSelect}/>
+      </div>
+      <div className="mt-10 px-36">
+        <Forms_Room selectedRow={selectedRow}/>
+      </div>
+    </div>
+  );
+}
